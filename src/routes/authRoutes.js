054ea7ba101cc13fda84aa2b728310cd52a617bb
Patrick Odeh <vyltyan@gmail.com>
2025-04-04 10:26:56 +0100

    Modified register route to return token during user registeration

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -28,7 +28,7 @@ router.post("/register", [
         user = new User({ name, email, password: hashedPassword });
         await user.save();
 
-        const token = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn:"1h"});
+        const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn:"1h"});
         res.json({ token });
     } catch (error) {
         res.status(500).json({ msg: "Server Error"});
